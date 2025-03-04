d4d98504ee644e65cff7fa0e85da61fb0bc93499
张炎泼 <drdr.xp@gmail.com>
2025-03-04 21:52:13 +0800

    M  src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,18 @@ impl Container for () {
     type Item = Foo;
 }
 
-pub struct Foo;
+struct Foo;
 
 impl Default for <() as Container>::Item {
     fn default() -> Self {
         Foo
+    }
+}
+
+struct Bar;
+
+impl Default for Bar {
+    fn default() -> Self {
+        Bar
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+// Make main.rs depends on lib.rs
 use t_conflict::Container as _;
 
 fn main() {}
-pub struct Bar;
 
-impl Default for Bar {
+struct Wow;
+impl Default for Wow {
     fn default() -> Self {
-        Bar
+        Wow
     }
 }
